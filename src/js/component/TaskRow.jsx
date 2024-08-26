a2bbe8db936bafbccc0a481cdab627535233eaef
Christian_Caballero <94983984+xtiancr78@users.noreply.github.com>
2024-08-26 02:31:19 +0000

    avance

---
+++ src/js/component/TaskRow.jsx
@@ -0,0 +1,16 @@
+export default function TaskRow({ task, toggleTask }) {
+    return (
+      <tr key={task.name}>
+        <td className="d-flex justify-content-between">
+          {task.name}
+          <input
+            type="checkbox"
+            className="form-check-input"
+            checked={task.done}
+            onChange={() => toggleTask(task)}
+          />
+        </td>
+      </tr>
+    );
+  }
+  
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,20 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Home from "./Home.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // include your styles into the webpack bundle
-import "../styles/index.css";
+import "../../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
 
 //render your react application
 ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+
+
+
+
+
+
+
 
